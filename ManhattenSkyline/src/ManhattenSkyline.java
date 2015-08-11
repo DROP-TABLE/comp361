2fab94836abe014ff23cd9411798e3bb9201dc72
Daniel Little <littledani1@cafe-bodega.ecs.vuw.ac.nz>
2015-08-12 11:53:00 +1200

    added loading, saving, baro and test generation

--- ManhattenSkyline/src/ManhattenSkyline.java
+++ ManhattenSkyline/src/ManhattenSkyline.java
@@ -1,13 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 
 public class ManhattenSkyline {
 
+	private List<Building> city;
+
+	private int barometer = 0;
 
 	public ManhattenSkyline(){
-
+		city = load();
+		if(city != null){
+			List<SkylineTuple> skyline = buildSkyline(city, 0, city.size()-1);
+			//display(skyline);
+			save(skyline);
+			System.out.println("Barometer: " + barometer);
+		}
 	}
+
+
 
 	public List<SkylineTuple> buildSkyline(List<Building> input, int start, int end){
 		if(start == end){
@@ -34,6 +55,7 @@ public class ManhattenSkyline {
 		double x = 0;
 		List<SkylineTuple> skyline = new ArrayList<>();
 		while(i < sky1.size() && j < sky2.size()){
+			barometer++;
 			if(sky1.get(i).getX() < sky2.get(j).getX()){
 				SkylineTuple elem = sky1.get(i);
 				h1 = elem.getHeight();
@@ -53,6 +75,7 @@ public class ManhattenSkyline {
 			}
 		}
 		while(i < sky1.size()){
+			barometer++;
 			SkylineTuple elem = sky1.get(i);
 			i++;
 			if(ho != elem.getHeight()){
@@ -60,6 +83,7 @@ public class ManhattenSkyline {
 			}
 		}
 		while(j < sky2.size()){
+			barometer++;
 			SkylineTuple elem = sky2.get(j);
 			j++;
 			if(ho != elem.getHeight()){
@@ -70,30 +94,67 @@ public class ManhattenSkyline {
 		return skyline;
 	}
 
-
-	/*public List<SkylineTuple> buildSkyline(List<Building> input){
-		stack.push(new StackTuple(0, input.size()));
-		while(!stack.isEmpty()){
-			StackTuple next = stack.pop();
-			int start = next.getStart();
-			int end = next.getEnd();
-
-			if(start == end){
-				Building elem = input.get(start);
-				List<SkylineTuple> skyline = new ArrayList<>();
-				skyline.add(new SkylineTuple(elem.getLeft(), elem.getHeight()));
-				skyline.add(new SkylineTuple(elem.getRight(), 0));
-				return skyline;
-			}
-			else{
-				stack.push(new StackTuple(start, (start+end)/2));
-				stack.push(new StackTuple((start+end)/2 + 1, end));
-			}
-
-
+	private void display(List<SkylineTuple> skyline){
+		for(SkylineTuple tuple : skyline){
+			System.out.println("(" + tuple.getX() + ", " + tuple.getHeight() + ")");
 		}
-		return null;
-	}*/
+	}
 
+	private void save(List<SkylineTuple> skyline){
+		try{
+			File file = new File("output.txt");
+			if(file.exists()){ //removes file if exists to clear.
+				file.delete();
+			}
+			file.createNewFile();
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			for(SkylineTuple tuple : skyline){
+				bw.write("(" + tuple.getX() + ", " + tuple.getHeight() + ")");
+				bw.newLine();
+			}
+			bw.close();
+			System.out.println("File Created");
+
+		}catch(IOException e){System.out.println("Failed to write to file " + e);}
+	}
+
+
+
+	public List<Building> load(){
+
+		final JFrame frame = new JFrame();
+		final JFileChooser fileChooser = new JFileChooser();
+		File file = null;
+		fileChooser.setCurrentDirectory(new File("."));
+		fileChooser.setDialogTitle("Select input file");
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+		if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+			file = fileChooser.getSelectedFile();
+		}
+		else {return null; }
+
+		try{
+			List<Building> city = new ArrayList<>();
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			String[] tokens;
+			while((line = br.readLine()) != null){
+				tokens = line.split(" ");
+				city.add(new Building(Double.parseDouble(tokens[0]), Double.parseDouble(tokens[1]), Double.parseDouble(tokens[2])));
+			}
+			br.close();
+			return city;
+		}
+		catch(IOException e){
+			System.out.println("IO exception on file load: " + e);
+			return null;
+		}
+	}
+
+	public static void main(String[] args){
+		new ManhattenSkyline();
+		System.exit(0);
+	}
 
 }
---
+++ ManhattenSkyline/src/TestCityGen.java
@@ -0,0 +1,119 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestCityGen {
+
+	/**
+	 * NORMAL: Standard random generation using Math.random()
+	 * DISTINCT: Prevents overlapping buildings
+	 * NESTED: Buildings edges do not cross. Each is inside others.
+	 */
+
+	private enum Mode {NORMAL, DISTINCT, NESTED}
+
+	private final Mode MODE = Mode.NESTED;
+	private final int SIZE = 10000000;
+	private final String FILENAME = MODE.toString() + SIZE;
+
+
+	public TestCityGen(){
+		List<Building> city = generate(SIZE, MODE);
+		writeToFile(city, FILENAME);
+	}
+
+
+	private void writeToFile(List<Building> city, String filename){
+		try{
+			File file = new File("tests/" + filename);
+			if(file.exists()){
+				System.out.println("File already exists");
+				return;
+			}
+			else{
+				file.createNewFile();
+			}
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+			for(Building b : city){
+				bw.write(b.getLeft() + " " + b.getRight() + " " + b.getHeight());
+				bw.newLine();
+			}
+			bw.close();
+
+
+		}catch(IOException e){System.out.println("Error in writing to file " + e);}
+	}
+
+	private List<Building> generate(int size, Mode mode){
+		List<Building> city = new ArrayList<>();
+
+		double left = 0;
+		double right = 0;
+		double height = 0;
+
+		switch(mode){
+		case NORMAL:
+			for(int i=0;i<size;i++){
+				left = Math.random()*1000;
+				right = left + (Math.random()*50);
+				height = Math.random()*200;
+				city.add(new Building(left, right, height));
+			}
+			break;
+
+		case DISTINCT:
+			for(int i=0;i<size;i++){
+				left = right + Math.random()*20;
+				right = left + (Math.random()*50);
+				height = Math.random()*200;
+				city.add(new Building(left, right, height));
+			}
+			break;
+
+		case NESTED:
+			for(int i=0;i<size;i++){
+				left = left - Math.random()*10;
+				right = right + (Math.random()*10);
+				height = height + Math.random()*20;
+				city.add(new Building(left, right, height));
+			}
+			break;
+		}
+		System.out.println("File Created");
+		return city;
+	}
+
+
+	private void generateTestSet(int max){
+		int num = 10;
+		while(num < max){
+			build(num, Mode.NORMAL, "NORMAL" + num);
+			build(num, Mode.DISTINCT, "DISTINCT" + num);
+			build(num, Mode.NESTED, "NESTED" + num);
+			num = num*10;
+		}
+
+	}
+
+	private void build(int size, Mode m, String name){
+		List<Building> city = generate(SIZE, MODE);
+		writeToFile(city, FILENAME);
+	}
+
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+		new TestCityGen();
+		System.exit(0);
+	}
+
+}
